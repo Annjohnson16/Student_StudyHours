792a4a0276d59767c603eb4cd94969a8f364164d
Annjohnson16 <148953632+Annjohnson16@users.noreply.github.com>
2026-09-17 11:02:21 +0530

    Enhance prediction with attendance input
    Updated model loading and added attendance input for prediction.

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 import streamlit as st
 import joblib
 
-model = joblib.load("Logistic_Regression1.pkl")
+model = joblib.load("Logistic_Regression.pkl")
 
-st.title("Student Pass/Fail based on Study Hours")
+st.title("Student Pass/Fail based on Study Hours and Attendance")
 
-hours = st.number_input("Enter Study Hours", min_value = 0.0, max_value = 15.0, value = 5.0)
+hours = st.number_input(
+    "Enter Study Hours",
+    min_value=0.0,
+    max_value=15.0,
+    value=5.0
+)
+
+attendance = st.number_input(
+    "Enter Attendance",
+    min_value=0.0,
+    max_value=100.0,
+    value=75.0
+)
 
 if st.button("Predict"):
-  prediction = model.predict([[hours]])
 
-  if prediction[0] == 1:
-    st.success("PASS")
-  else:
-    st.error("FAIL")
+    # Both features in the same row
+    prediction = model.predict([[hours, attendance]])
+
+    if prediction[0] == 1:
+        st.success("PASS")
+    else:
+        st.error("FAIL")
